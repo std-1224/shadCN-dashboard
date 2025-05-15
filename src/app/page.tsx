f6197958ead4a929805ca52ddb9719d0d66632cb
std-1224 <kalkaudy@gmail.com>
2025-05-15 10:57:57 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import General from "@/components/Cards/General";
+
+export default function Home() {
+  return <div>
+    <div className="grid grid-cols-2">
+      <General />
+    </div>
+  </div>
+}
